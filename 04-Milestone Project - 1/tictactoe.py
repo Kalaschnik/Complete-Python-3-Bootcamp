2151b88182e1b7103af990a437ad49096b798eae
= <steven.kalinke@outlook.com>
2020-03-01 14:38:13 +0100

    Complete Section 3 and 4

--- 04-Milestone Project - 1/tictactoe.py
+++ 04-Milestone Project - 1/tictactoe.py
@@ -1,49 +1,104 @@
-print("Welcome to PyMoji Tic Tac Toe")
-# print(
-#     """
-# ❌  ⭕  🕳️
-# ⭕  🕳️  ❌
-# 🕳️  ⭕  ❌
-# """
-# )
+import tictactoe_functions
 
-# print(
-#     """
-# 1  2  3
-# 4  5  6
-# 7  8  9
-# """
-# )
+print("+++ Welcome to PyMoji Tic Tac Toe +++")
 
-character_mapping = {"x": "❌", "o": "⭕", "-": "🕳️"}
+character_mapping = {1: "❌", 0: "⭕", "-": "🕳️"}
 
-field_state = {
-    1: character_mapping["-"],
-    2: character_mapping["-"],
-    3: character_mapping["-"],
-    4: character_mapping["-"],
-    5: character_mapping["-"],
-    6: character_mapping["-"],
-    7: character_mapping["-"],
-    8: character_mapping["-"],
-    9: character_mapping["-"],
+winning_patterns = (
+    (1, 2, 3),
+    (4, 5, 6),
+    (7, 8, 9),
+    (1, 4, 7),
+    (2, 5, 8),
+    (3, 6, 9),
+    (1, 5, 9),
+    (3, 5, 7),
+)
+
+state = {
+    "field": {
+        1: "🕳️",
+        2: "🕳️",
+        3: "🕳️",
+        4: "🕳️",
+        5: "🕳️",
+        6: "🕳️",
+        7: "🕳️",
+        8: "🕳️",
+        9: "🕳️",
+    },
+    "player_x": [],
+    "player_y": [],
+    "round": 1,
+    "is_running": True,
 }
 
-print(
-    f"{field_state[1]}  {field_state[2]}  {field_state[3]}\n"
-    f"{field_state[4]}  {field_state[5]}  {field_state[6]}\n"
-    f"{field_state[7]}  {field_state[8]}  {field_state[9]}"
-)
 
-print("Player ❌  always goes first.")
-input_playerx = int(input("Enter your number: "))
-print(input_playerx)
+def get_current_player(current_round):
+    if current_round % 2 == 1:
+        return "player_x"
+    if current_round % 2 == 0:
+        return "player_y"
 
-# update field state
-field_state[input_playerx] = "❌"
 
-print(
-    f"{field_state[1]}  {field_state[2]}  {field_state[3]}\n"
-    f"{field_state[4]}  {field_state[5]}  {field_state[6]}\n"
-    f"{field_state[7]}  {field_state[8]}  {field_state[9]}"
-)
+def is_free(position):
+    return state["field"][position] == "🕳️"
+
+
+def append_position(position):
+    if state["round"] % 2 == 1:
+        state["player_x"].append(position)
+    if state["round"] % 2 == 0:
+        state["player_y"].append(position)
+
+
+def is_winning(sequence):
+    for winning_pattern in winning_patterns:
+        if set(winning_pattern) <= set(sequence):
+            state["is_running"] = False
+            return True
+
+    return False
+
+
+def update_field(position):
+    state["field"][position] = character_mapping[state["round"] % 2]
+
+
+def print_pretty_field():
+    print(
+        f"{state['field'][1]}  {state['field'][2]}  {state['field'][3]}   ➡  1 2 3\n"
+        f"{state['field'][4]}  {state['field'][5]}  {state['field'][6]}   ➡  4 5 6\n"
+        f"{state['field'][7]}  {state['field'][8]}  {state['field'][9]}   ➡  7 8 9"
+    )
+
+
+print("The field is coded into a number grid, like so:")
+print_pretty_field()
+
+
+while state["is_running"]:
+    print(f"\n+++++ ROUND {state['round']} +++++")
+    print(f"Player {character_mapping[state['round'] % 2]} . Your turn...")
+    current_player = get_current_player(state["round"])
+
+    while True and state["round"] < 10:
+        ui_position = tictactoe_functions.sanitised_input(
+            "Enter your position: ", int, 1, 9
+        )
+        if not is_free(ui_position):
+            print("This spot is already occupied. Choose a different spot.")
+
+        if is_free(ui_position):
+            break
+
+    append_position(ui_position)
+    update_field(ui_position)
+    print_pretty_field()
+
+    if is_winning(state[current_player]):
+        print(
+            f"Player {character_mapping[state['round'] % 2]}  won the match! Congratulations!"
+        )
+
+    state["round"] += 1
